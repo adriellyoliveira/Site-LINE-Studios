af307e0f7d10918cb81fc61d5254dbd16634c878
Adrielly Oliveira Araújo <adriellyaraujo826@gmail.com>
2024-02-10 17:33:13 -0300

    Atualização

--- script.js
+++ script.js
@@ -8,11 +8,33 @@ setInterval(function(){
 
 function nextImage(){
     count++;
-    if(count>4){
+    if(count>5){
         count = 1;
     }
     
     document.getElementById("radio"+count).checked = true;
 
 }
+
+
+
+
+
+
+function toggleArrow() {
+    var arrow = document.getElementById("arrow");
+    var dropdown = document.querySelector(".dropdown");
+    var dropdownStyle = window.getComputedStyle(dropdown);
+
+    if (dropdownStyle.display === "block") {
+        dropdown.style.display = "none";
+        arrow.innerHTML = "&#9662;"; // Define a seta para baixo quando o menu está fechado
+    } else {
+        dropdown.style.display = "block";
+        arrow.innerHTML = "&#9652;"; // Define a seta para cima quando o menu está aberto
+    }
+}
+
+
+
 
